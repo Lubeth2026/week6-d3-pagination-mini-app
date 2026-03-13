7b7335428f638f798deb937b3b0e3c8f5e8c747e
Lubeth <lubethjimenez@yahoo.com>
2026-03-13 19:20:27 -0400

    Add prev/next buttons to eventListener

--- app.js
+++ app.js
@@ -2,3 +2,25 @@
 
 console.log("Hello Everyone!");
 
+//Global Variables for getting elements by ID//
+const outpput = document.querySelector("#output");
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+
+//Main Function//
+async function main() {
+    try {
+        console.log("Ready to fetch")
+        prev.addEventListener("click", async()=>{
+        
+        })
+
+        next.addEventListener("click", async()=>{
+
+        })
+
+    } catch (error) {
+        
+    }
+}
+main()

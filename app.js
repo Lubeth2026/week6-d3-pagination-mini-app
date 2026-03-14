b717d7ec93c321526b613209002e9b8c76b2023a
Lubeth <lubethjimenez@yahoo.com>
2026-03-13 20:28:50 -0400

    Add fetch page load with a message

--- app.js
+++ app.js
@@ -7,16 +7,47 @@ const outpput = document.querySelector("#output");
 const prev = document.querySelector("#prev");
 const next = document.querySelector("#next");
 
+
+let page = 1
+const itemsPerPage = 20;
+
+//Get data from the Rick & Morty API//
+async function getData(url) {
+    try {
+        const res = await fetch(url)
+
+        if(res.status !== 200){
+            throw new Error("Failed to fetch")
+        }
+
+        const info = await res.json()
+        return info;
+    } catch (error) {
+        console.error(error) 
+    }
+}
+
+function render(){
+    console.log("Loading fetching your info")
+}
+
 //Main Function//
 async function main() {
     try {
+        //Get data on page load//
+        const info = await getData("https://rickandmortyapi.com/api/character/?page" + page);
+
         console.log("Ready to fetch")
         prev.addEventListener("click", async()=>{
-        
+        const info = await getData("https://rickandmortyapi.com/api/character/?page" + page);
+        console.log(info)
+        render()
         })
 
         next.addEventListener("click", async()=>{
-
+        const info = await getData("https://rickandmortyapi.com/api/character/?page" + page);
+        console.log(info)
+        render()
         })
 
     } catch (error) {
